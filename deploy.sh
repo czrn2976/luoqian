0e9122c1cc3933cc68f16bc1b106affc65087106
app-harry <harry.li@kkday.com>
2024-10-27 20:56:05 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -58,8 +58,8 @@ deploy() {
 
 main() {
   init     # Initialize and validate environment
-  setup_gh # Set up the gh-pages branch
   build    # Build the site
+  setup_gh # Set up the gh-pages branch
   flush
   deploy   # Deploy the site
 }
